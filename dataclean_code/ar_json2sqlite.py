8a3804375bf90534de9149db1fa1fcd35184b08d
Ruiqi (Richard) Chen <128464367+Ruiqi-Chen-0216@users.noreply.github.com>
2025-06-24 17:17:51 -0700

    update part of ar manual code

--- dataclean_code/ar_json2sqlite.py
+++ dataclean_code/ar_json2sqlite.py
@@ -2,17 +2,17 @@ import pandas as pd
 import sqlite3
 
 # 读取 JSON 文件
-df = pd.read_json("../query_bookreview\original_dataset/review_query_remapped.json", lines=True)
+df = pd.read_json("../query_bookreview\origin_dataset/review_query_remapped.json", lines=True)
 
 # ✅ 强制将 review_time 转换为字符串（防止 SQLite 自动识别为时间戳）
 if "review_time" in df.columns:
     df["review_time"] = df["review_time"].astype(str)
 
 # 创建 SQLite 连接
-conn = sqlite3.connect("../query_bookreview\original_dataset/review_query.db")
+conn = sqlite3.connect("../query_bookreview\origin_dataset/review_query.db")
 
 # 写入 SQLite，表名为 meta
-df.to_sql("meta", conn, if_exists="replace", index=False)
+df.to_sql("review", conn, if_exists="replace", index=False)
 
 # 关闭连接
 conn.close()
