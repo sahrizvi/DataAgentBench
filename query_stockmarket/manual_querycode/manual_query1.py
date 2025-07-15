891a73396a964606ebdf150b45c4598062e50903
Ruiqi (Richard) Chen <128464367+Ruiqi-Chen-0216@users.noreply.github.com>
2025-07-15 14:15:59 -0700

    update manual code

--- query_stockmarket/manual_querycode/manual_query1.py
+++ query_stockmarket/manual_querycode/manual_query1.py
@@ -3,11 +3,13 @@ import duckdb
 import pandas as pd
 
 from openai import AzureOpenAI       
+import os
 client = AzureOpenAI(
-    api_key="609ced4d971240b8a08f7fb0e6d846ea",
-    api_version="2024-08-01-preview",
-    azure_endpoint="https://promptdelta-nc.openai.azure.com",
-)
+        api_key=os.getenv("AZURE_API_KEY"),
+        api_version=os.getenv("AZURE_API_VERSION", "2023-05-15"),
+        azure_endpoint=os.getenv("AZURE_API_BASE")
+    )
+
 deployment_name = "gpt-4o"
 
 # Step 1: Extract all unique symbols from SQLite
