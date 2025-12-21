ffafd36cfc2a3d9ebe32ea66b1913fc8cf1b7bd8
Ruiying-Ma <ruiying021120@gmail.com>
2025-12-20 16:25:01 -0800

    update

--- python_script/collect_traces.py
+++ python_script/collect_traces.py
@@ -8,20 +8,24 @@ import json
 import shutil
 
 RESULT_ROOT = Path("/home/ruiying/DataAgentBench")
-DST_ROOT = Path("/home/ruiying/DataAgentBench/results-gemini-3-pro-old")
+# DST_ROOT = Path("/home/ruiying/DataAgentBench/results-gemini-2.5-flash")
+DST_ROOT = Path("/home/ruiying/DataAgentBench/results-gemini-3-pro")
+# DST_ROOT = Path("/home/ruiying/DataAgentBench/results-gpt5.1")
 
 if __name__ == "__main__":
     for task in [
-        "bookreview",
-        "crmarenapro",
-        "DEPS_DEV_V1",
-        "GITHUB_REPOS",
-        "googlelocal",
-        "PANCANCER_ATLAS",
-        "PATENTS",
-        "stockindex",
-        "stockmarket",
-        "yelp"
+        # "bookreview",
+        # "crmarenapro",
+        # "DEPS_DEV_V1",
+        # "GITHUB_REPOS",
+        # "googlelocal",
+        # "PANCANCER_ATLAS",
+        # "PATENTS",
+        # "stockindex",
+        # "stockmarket",
+        # "yelp",
+        "agnews",
+        "music_brainz_20k",
     ]:
         query_dir = RESULT_ROOT / f"query_{task}"
         for folder_name in sorted(os.listdir(query_dir)):
@@ -31,9 +35,14 @@ if __name__ == "__main__":
                     # print(query_id)
                 except Exception as e:
                     continue
-                result_dir = query_dir / folder_name / "logs"
+                if "gpt5.1" in DST_ROOT.name:
+                    result_dir = query_dir / folder_name / "logs" / "data_agent"
+                else:
+                    result_dir = query_dir / folder_name / "logs"
                 if result_dir.exists():
                     print(f"Copying results for task {task}, query {query_id}...")
-                    shutil.copytree(result_dir, DST_ROOT / f"query_{task}" / folder_name)
+                    dst_dir = DST_ROOT / f"query_{task}" / folder_name
+                    assert not dst_dir.exists(), f"Destination directory {dst_dir} already exists!"
+                    shutil.copytree(result_dir, dst_dir)
 
         print("\n\n")
